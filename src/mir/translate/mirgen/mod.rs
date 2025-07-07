c21ba26bfffae6ec2fecf14e4a7c3e7733f69528
medihbt <rtevol0086@outlook.com>
2025-07-07 20:43:04 +0800

    modified:   src/mir/translate/mirgen/mod.rs

--- src/mir/translate/mirgen/mod.rs
+++ src/mir/translate/mirgen/mod.rs
@@ -50,14 +50,36 @@ struct MirTranslateCtx {
 }
 
 struct MirBlockInfo {
-    pub ir:  BlockRef,
+    pub ir: BlockRef,
     pub mir: MirBlockRef,
+    pub instl: u32,
+    pub instr: u32,
 }
 
 struct InstTranslateInfo {
     pub ir: InstRef,
     pub ty: ValTypeID,
     pub kind: InstDataKind,
+}
+
+struct AllocaInfo {
+    pub ir: InstRef,
+    pub align_log2: u8,
+    pub pointee_ty: ValTypeID,
+}
+
+struct InstTranslateMap {
+    blocks: Vec<MirBlockInfo>,
+    insts: Vec<InstTranslateInfo>,
+    allocas: Vec<AllocaInfo>,
+}
+
+impl InstTranslateMap {
+    fn block_get_insts(&self, block_info: &MirBlockInfo) -> &[InstTranslateInfo] {
+        let start = block_info.instl as usize;
+        let end = block_info.instr as usize;
+        &self.insts[start..end]
+    }
 }
 
 impl MirTranslateCtx {
@@ -98,50 +120,23 @@ impl MirTranslateCtx {
     ) {
         // Step 1.1 为每个函数的基本块分配 MIR 块引用
         let dfs_seq = CfgDfsSeq::new_from_snapshot(cfg, DfsOrder::Pre);
-        let block_map = self.alloc_mirref_for_blocks(Rc::clone(mir_func), dfs_seq);
+        let mut block_map = self.alloc_mirref_for_blocks(Rc::clone(mir_func), dfs_seq);
 
         // Step 1.2 处理每个基本块的前驱和后继关系
-        for &MirBlockInfo { ir, mir } in &block_map {
+        for &MirBlockInfo { ir, mir, .. } in &block_map {
             self.complete_mir_block_succ_and_pred(cfg, ir, mir, &block_map);
         }
 
-        // Step 1.3 为函数确定参数布局和栈布局
-        let mut allocas = Vec::new();
-        let mut all_insts = Vec::new();
-
-        todo!("build a block-to-inst mapping for MIR translation");
-
-        for &MirBlockInfo { ir, mir: _ } in &block_map {
-            let (insts, len) = self
-                .ir_module
-                .get_block(ir)
-                .instructions
-                .load_range_and_length();
-            all_insts.reserve(len);
-            let alloc_value = self.ir_module.borrow_value_alloc();
-            let alloc_inst = &alloc_value.alloc_inst;
-            for (ir, inst) in insts.view(alloc_inst) {
-                if let InstData::Alloca(_, a) = inst {
-                    allocas.push((ir, a.align_log2, a.pointee_ty));
-                }
-                let ty = inst.get_value_type();
-                let kind = inst.get_kind();
-                type K = InstDataKind;
-                if matches!(kind, K::ListGuideNode | K::PhiInstEnd | K::Intrin) {
-                    continue; // 跳过不需要翻译的指令
-                }
-                all_insts.push(InstTranslateInfo { ir, ty, kind });
-            }
-        }
-
-        todo!("handle allocas for MIR function stack layout");
+        // Step 1.3 dump 出所有指令, 并为函数确定参数布局和栈布局
+        let inst_map = self.dump_insts_and_layout(block_map);
 
         // Step 1.? 翻译每个基本块的指令
-        for &MirBlockInfo { ir, mir } in &block_map {
-            self.inst_dispatch_for_one_mir_block(ir, mir, mir_func, globals, &block_map);
+        for &MirBlockInfo { ir, mir, .. } in &inst_map.blocks {
+            self.inst_dispatch_for_one_mir_block(ir, mir, mir_func, globals, &inst_map.blocks);
         }
     }
 
+    /// Step 1.1: 为每个函数的基本块分配 MIR 块引用
     fn alloc_mirref_for_blocks(&mut self, func: Rc<MirFunc>, dfs: CfgDfsSeq) -> Vec<MirBlockInfo> {
         let mut block_map = Vec::with_capacity(dfs.nodes.len());
         for (idx, node) in dfs.nodes.iter().enumerate() {
@@ -158,6 +153,8 @@ impl MirTranslateCtx {
             block_map.push(MirBlockInfo {
                 ir: bb,
                 mir: mir_bb_ref,
+                instl: 0,
+                instr: 0,
             });
             let mut mir_builder = MirBuilder::new(&mut self.mir_module);
             mir_builder.set_focus(MirFocus::Func(Rc::clone(&func)));
@@ -167,6 +164,7 @@ impl MirTranslateCtx {
         block_map
     }
 
+    /// Step 1.2: 设置 MIR 基本块的后继和前驱关系
     fn complete_mir_block_succ_and_pred(
         &self,
         cfg: &CfgSnapshot,
@@ -199,6 +197,60 @@ impl MirTranslateCtx {
         }
     }
 
+    /// Step 1.3: Dump 出所有指令, 并为函数确定参数布局和栈布局
+    fn dump_insts_and_layout(&mut self, mut block_map: Vec<MirBlockInfo>) -> InstTranslateMap {
+        let mut insts = Vec::new();
+        let mut allocas = Vec::new();
+
+        let mut inst_cnt = 0;
+
+        for info in block_map.iter_mut() {
+            let MirBlockInfo {
+                ir,
+                instl: inst_begin,
+                instr: inst_end,
+                ..
+            } = info;
+
+            let (insts_in_block, len) = self
+                .ir_module
+                .get_block(*ir)
+                .instructions
+                .load_range_and_length();
+            insts.reserve(len);
+
+            let alloc_value = self.ir_module.borrow_value_alloc();
+            let alloc_inst = &alloc_value.alloc_inst;
+
+            *inst_begin = inst_cnt;
+            for (ir, inst) in insts_in_block.view(alloc_inst) {
+                if let InstData::Alloca(_, a) = inst {
+                    allocas.push(AllocaInfo {
+                        ir,
+                        align_log2: a.align_log2,
+                        pointee_ty: a.pointee_ty,
+                    });
+                }
+                let ty = inst.get_value_type();
+                let kind = inst.get_kind();
+                type K = InstDataKind;
+                if matches!(kind, K::ListGuideNode | K::PhiInstEnd | K::Intrin) {
+                    continue; // 跳过不需要翻译的指令
+                }
+                insts.push(InstTranslateInfo { ir, ty, kind });
+                inst_cnt += 1;
+            }
+            *inst_end = inst_cnt;
+        }
+
+        InstTranslateMap {
+            blocks: block_map,
+            insts,
+            allocas,
+        }
+    }
+
+    /// Step 1.?: 翻译每个基本块的指令
     fn inst_dispatch_for_one_mir_block(
         &mut self,
         ir_bb: BlockRef,
